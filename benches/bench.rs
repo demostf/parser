b09d0507d68a854e7989081ccaaceca44eb63271
Robin Appelman <robin@icewind.nl>
2019-03-17 19:30:46 +0100

    add benchmarks

---
+++ benches/bench.rs
@@ -0,0 +1,30 @@
+#![feature(test)]
+
+extern crate test;
+
+use std::fs;
+
+use test::Bencher;
+use tf_demo_parser::{Demo, Stream, DemoParser};
+
+fn bench_file(input_file: &str, b: &mut Bencher) {
+    let file = fs::read(input_file).expect("Unable to read file");
+    let demo = Demo::new(file);
+    let stream: Stream = demo.get_stream();
+
+    b.iter(|| {
+        let parser = DemoParser::new(stream.clone());
+        let (_, state) = parser.parse_demo().unwrap();
+        test::black_box(state);
+    })
+}
+
+#[bench]
+fn bench_gully(b: &mut Bencher) {
+    bench_file("data/gully.dem", b);
+}
+
+#[bench]
+fn bench_comp(b: &mut Bencher) {
+    bench_file("data/comp.dem", b);
+}
