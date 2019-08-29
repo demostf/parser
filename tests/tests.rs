eb934340174892883a391451f1fe486c4381a680
Robin Appelman <robin@icewind.nl>
2019-08-29 23:30:20 +0200

    dont dump in test

--- tests/tests.rs
+++ tests/tests.rs
@@ -32,15 +32,6 @@ fn test_message_types(input_file: &str, snapshot_file: &str) {
     assert_eq!(expected, message_types);
 }
 
-fn dump_message_types(input_file: &str, snapshot_file: &str) {
-    let file = fs::read(input_file).expect("Unable to read file");
-    let demo = Demo::new(file);
-    let (_, message_types) =
-        DemoParser::parse_with_analyser(demo.get_stream(), MessageTypeAnalyser::new()).unwrap();
-
-    fs::write(snapshot_file, serde_json::to_vec(&message_types).unwrap()).unwrap();
-}
-
 #[test]
 fn snapshot_test_small() {
     snapshot_test("data/small.dem", "data/small.json");
@@ -64,9 +55,4 @@ fn snapshot_test_malformed_cvar() {
 #[test]
 fn snapshot_test_decal() {
     snapshot_test("data/decal.dem", "data/decal.json");
-}
-
-#[test]
-fn message_type_test_comp() {
-    dump_message_types("data/comp.dem", "data/comp_message_types.json");
 }
