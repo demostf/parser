3df547111c6f147a99ebfaa172dd22428bd123e3
Robin Appelman <robin@icewind.nl>
2023-12-23 17:25:25 +0100

    flake updates

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
   inputs = {
-    nixpkgs.url = "nixpkgs/release-23.05";
+    nixpkgs.url = "nixpkgs/release-23.11";
     flake-utils.url = "github:numtide/flake-utils";
-    naersk.url = "github:icewind1991/naersk?rev=6d245a3bbb2ee31ec726bb57b9a8b206302e7110";
+    naersk.url = "github:nix-community/naersk";
     rust-overlay.url = "github:oxalica/rust-overlay";
     rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
     rust-overlay.inputs.flake-utils.follows = "flake-utils";
@@ -19,9 +19,11 @@
     nixpkgs,
     rust-overlay,
     ...
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      overlays = [ (import rust-overlay) ];
+  }: let
+      inherit (flake-utils.lib) eachDefaultSystem eachSystem;
+    in
+    (eachDefaultSystem (system: let
+      overlays = [(import rust-overlay)];
       pkgs = (import nixpkgs) {
         inherit system overlays;
       };
@@ -50,34 +52,60 @@
       };
 
       buildMatrix = targets: {
-        include = builtins.map (target: {
-          inherit target;
-          artifact_name = artifactForTarget target;
-          asset_name = assetNameForTarget target;
-        }) targets;
+        include =
+          builtins.map (target: {
+            inherit target;
+            artifact_name = artifactForTarget target;
+            asset_name = assetNameForTarget target;
+          })
+          targets;
       };
       hostNaersk = cross-naersk'.hostNaersk;
-    in rec {
-      packages = lib.attrsets.genAttrs targets (target: (cross-naersk'.buildPackage target) (nearskOpt // {
-        overrideMain = args: args // {
-          preConfigure = ''
-            cargo_build_options="$cargo_build_options --bin parse_demo"
-          '';
+
+      mkHydraJobs = system: {
+        parser = derivation {
+          name = "parser";
+          builder = "mybuilder";
+          inherit system;
         };
-      })) // rec {
-        tf-demo-parser = packages.${hostTarget};
-        check = hostNaersk.buildPackage (nearskOpt // {
-          mode = "check";
-        });
-        clippy = hostNaersk.buildPackage (nearskOpt // {
-          mode = "clippy";
-        });
-        test = hostNaersk.buildPackage (nearskOpt // {
-          release = false;
-          mode = "test";
-        });
-        default = tf-demo-parser;
+        nested = {
+          attribute = derivation {
+            name = "nested-attribute";
+            builder = "mybuilder";
+            inherit system;
+          };
+        };
       };
+    in rec {
+      packages =
+        lib.attrsets.genAttrs targets (target:
+          (cross-naersk'.buildPackage target) (nearskOpt
+            // {
+              overrideMain = args:
+                args
+                // {
+                  preConfigure = ''
+                    cargo_build_options="$cargo_build_options --bin parse_demo"
+                  '';
+                };
+            }))
+        // rec {
+          tf-demo-parser = packages.${hostTarget};
+          check = hostNaersk.buildPackage (nearskOpt
+            // {
+              mode = "check";
+            });
+          clippy = hostNaersk.buildPackage (nearskOpt
+            // {
+              mode = "clippy";
+            });
+          test = hostNaersk.buildPackage (nearskOpt
+            // {
+              release = false;
+              mode = "test";
+            });
+          default = tf-demo-parser;
+        };
 
       inherit targets;
       inherit releaseTargets;
@@ -92,8 +120,29 @@
         default = tf-demo-parser;
       };
 
+      checks = {
+        fmt-check = pkgs.stdenvNoCC.mkDerivation {
+          name = "fmt-check";
+          src = ./.;
+          doCheck = true;
+          dontBuild = true;
+          nativeBuildInputs = with pkgs; [alejandra shellcheck shfmt];
+          checkPhase = ''
+            alejandra -c .
+          '';
+          installPhase = ''
+            mkdir $out
+          '';
+        };
+      };
+
       devShells.default = pkgs.mkShell {
         nativeBuildInputs = with pkgs; [rust-bin.stable.latest.default bacon cargo-edit cargo-outdated rustfmt clippy cargo-audit hyperfine valgrind cargo-insta cargo-semver-checks];
       };
+    })
+    // {
+      hydraJobs = eachSystem ["x86_64-linux" "aarch64-linux"] (system: {
+        parser = self.packages.${system}.tf-demo-parser;
+      });
     });
 }
